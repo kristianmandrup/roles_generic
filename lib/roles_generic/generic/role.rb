7a2f1dd224a704036b5b040a219279f9ecd6ac59
Kristian Mandrup <kmandrup@gmail.com>
2011-01-21 12:57:32 +0100

    refactored for late-binding of Role class

--- lib/roles_generic/generic/role.rb
+++ lib/roles_generic/generic/role.rb
@@ -6,17 +6,19 @@ module Roles::Generic::Role
   end
   
   module ClassMethods 
+    attr_accessor :class_constant
+            
     def role_class_name
-      @role_class_name          
-    end
-
-    def role_class class_constant
       const = class_constant.to_s.camelize
       begin
-        @role_class_name = "#{const}".constantize
+        @role_class_name ||= "#{const}".constantize
       rescue
         puts "Role class constant '#{const}' is not defined so it could not be set!"
       end
+    end
+
+    def role_class class_constant
+      @class_constant = class_constant  
     end
   end    
 end
